37aa07f0efd7746cb62716bf5bda9bfed5d5cd89
Nikola <ncurilovic@gmail.com>
2019-10-27 01:40:25 +0200

    Change version to 0.1.1

--- test/link_preview_test.dart
+++ test/link_preview_test.dart
@@ -1,21 +1,3 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:link_preview/link_preview.dart';
-
 void main() {
-  const MethodChannel channel = MethodChannel('link_preview');
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await LinkPreview.platformVersion, '42');
-  });
 }
