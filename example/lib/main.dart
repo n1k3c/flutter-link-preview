cacd881981eee572548331277fffe87fc6a90406
Nikola <ncurilovic@gmail.com>
2019-10-27 00:56:14 +0200

    Remove default example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:link_preview/link_preview.dart';
 
@@ -12,38 +12,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  static const EventChannel eventChannel = EventChannel('link_preview_events');
+  String _linkTitle = 'Unknown';
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
 
     getLinks();
   }
 
   Future<void> getLinks() async {
     try {
-      await LinkPreview.getPreview('https://www.coolinarika.com/recept/1094861/',
-          onData: (PreviewResponse data) => _handleData(data),
-          onError: (error) => _handleError(error));
-
-      await LinkPreview.getPreview('https://www.coolinarika.com',
-          onData: (PreviewResponse data) => _handleData(data),
-          onError: (error) => _handleError(error));
-
-      await LinkPreview.getPreview('https://www.google.com',
-          onData: (PreviewResponse data) => _handleData(data),
+      await LinkPreview.getPreview('https://flutter.dev',
+          onData: (PreviewResponse data) => _previewData(data),
           onError: (error) => _handleError(error));
     } on PlatformException {
       print('Error occured!!');
     }
   }
 
-  static _handleData(PreviewResponse previewResponse) {
+  _previewData(PreviewResponse previewResponse) {
     if (previewResponse.status == PreviewStatus.complete) {
+      _linkTitle = previewResponse.title;
       print('===============================================');
       print('Received status: ${previewResponse.status}');
       print('Received title: ${previewResponse.title}');
@@ -68,35 +58,15 @@ class _MyAppState extends State<MyApp> {
     print('===============================================');
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await LinkPreview.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Link preview example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('$_linkTitle\n'),
         ),
       ),
     );
--- lib/link_preview.dart
+++ lib/link_preview.dart
@@ -1,18 +1,10 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class LinkPreview {
-  static const MethodChannel _channel = const MethodChannel('link_preview');
-
   static const EventChannel eventChannel = EventChannel('link_preview_events');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
 
   static Future<String> getPreview(String url,
       {@required Function(PreviewResponse model) onData,
